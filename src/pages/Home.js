eb8eae884038fad6b8fca919b0f12355e398e12f
Krishnendu0016 <16kdas@gmail.com>
2024-02-08 01:36:20 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <section className="home-wrapper-1 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="main-banner position-relative p-3">
+                                <img src="images/main-banner-1.jpg" className='img-fluid rounded-3' alt="main banner" />
+                                <div className="main-banner-content position-absolute">
+                                    <h4>SUPERCHARGED FOR PROS.</h4>
+                                    <h5>iPad S13+ Pro</h5>
+                                    <p>From ₹1000000 or ₹4000/mo</p>
+                                    <Link>Buy Now</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6"></div>
+                    </div>
+                </div>
+            </section>
+
+        </>
     )
 }
 

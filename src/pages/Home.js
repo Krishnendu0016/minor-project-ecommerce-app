85ffdc04a4712a059bad6c173d8bd3bf7d1bb7ec
Krishnendu0016 <16kdas@gmail.com>
2024-02-08 12:22:36 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,45 +12,45 @@ const Home = () => {
                                 <div className="main-banner-content position-absolute">
                                     <h4>SUPERCHARGED FOR PROS.</h4>
                                     <h5>iPad S13+ Pro</h5>
-                                    <p>From ₹1000000 <br/> or ₹4000/mo</p>
+                                    <p>From ₹1000000 <br /> or ₹4000/mo</p>
                                     <Link className='button'>Buy Now</Link>
                                 </div>
                             </div>
                         </div>
                         <div className="col-6">
                             <div className='d-flex flex-wrap gap-10 justify-content-between align-items-center'>
-                            <div className="small-banner position-relative">
-                                <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
-                                <div className="small-banner-content position-absolute">
-                                    <h4>Best Sale!!</h4>
-                                    <h5>iPad S13+ Pro</h5>
-                                    <p>From ₹1000000 <br/> or ₹4000/mo</p>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>Best Sale!!</h4>
+                                        <h5>iPad S13+ Pro</h5>
+                                        <p>From ₹100000 <br /> or ₹4000/mo</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="small-banner position-relative">
-                                <img src="images/catbanner-02.jpg" className='img-fluid rounded-3' alt="main banner" />
-                                <div className="small-banner-content position-absolute">
-                                    <h4>NEW ARRIVAL</h4>
-                                    <h5>Buy IPad Air</h5>
-                                    <p>From ₹1000000 <br/> or ₹4000/mo</p>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-02.jpg" className='img-fluid rounded-3' alt="main banner" />
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>NEW ARRIVAL</h4>
+                                        <h5>Buy IPad Air</h5>
+                                        <p>From ₹100000 <br /> or ₹4000/mo</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="small-banner position-relative">
-                                <img src="images/catbanner-03.jpg" className='img-fluid rounded-3' alt="main banner" />
-                                <div className="small-banner-content position-absolute">
-                                    <h4>NEW ARRIVAL</h4>
-                                    <h5>Buy IPad Air</h5>
-                                    <p>From ₹1000000 <br/> or ₹4000/mo</p>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-03.jpg" className='img-fluid rounded-3' alt="main banner" />
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>NEW ARRIVAL</h4>
+                                        <h5>Buy IPad Air</h5>
+                                        <p>From ₹100000 <br /> or ₹4000/mo</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="small-banner position-relative">
-                                <img src="images/catbanner-04.jpg" className='img-fluid rounded-3' alt="main banner" />
-                                <div className="small-banner-content position-absolute">
-                                    <h4>NEW ARRIVAL</h4>
-                                    <h5>Buy IPad Air</h5>
-                                    <p>From ₹1000000 <br/> or ₹4000/mo</p>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-04.jpg" className='img-fluid rounded-3' alt="main banner" />
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>NEW ARRIVAL</h4>
+                                        <h5>Buy IPad Air</h5>
+                                        <p>From ₹100000 <br /> or ₹4000/mo</p>
+                                    </div>
                                 </div>
-                            </div>
                             </div>
                         </div>
                     </div>

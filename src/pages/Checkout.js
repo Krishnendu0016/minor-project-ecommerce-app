bb8eee476fed4ef2d72acf5dc3d8fbf7be3cee10
Krishnendu0016 <16kdas@gmail.com>
2024-03-09 11:05:44 +0530

    add utils

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from 'react-icons/bi'
 import watch from "../images/watch.jpg"
 const Checkout = () => {
     return (
@@ -10,37 +10,39 @@ const Checkout = () => {
                     <div className="col-7">
                         <div className="checkout-left-data">
                             <h3 className="website-name">5-friends Cornor</h3>
-                            <nav style={{"--bs-breadcrumb-divider": '>'}} 
-                            aria-label="breadcrumb">
+                            <nav style={{ "--bs-breadcrumb-divider": '>' }}
+                                aria-label="breadcrumb">
                                 <ol className="breadcrumb">
                                     <li className="breadcrumb-item">
-                                        <Link to="/cart" className="text-dark">Cart</Link>
-                                        </li>
-                                        &nbsp; /
-                                         <li className="breadcrumb-item active" aria-current="page">
+                                        <Link to="/cart" className="text-dark total-price">Cart</Link>
+                                    </li>
+                                    &nbsp; /&nbsp;
+                                    <li className="breadcrumb-item total-price active" aria-current="page">
                                         Information
                                     </li>
-                                    &nbsp; /<li className="breadcrumb-item active">
-                                       Shipping
+                                    &nbsp; /<li className="breadcrumb-item total-price active">
+                                        Shipping
                                     </li>
-                                        &nbsp; /
-                                    <li className="breadcrumb-item active" aria-current="page">
+                                    &nbsp; /
+                                    <li className="breadcrumb-item total-price active" aria-current="page">
                                         Payment
                                     </li>
                                 </ol>
                             </nav>
-                            <h4 className="title">Contact Information</h4>
-                            <p className="user-details">
+                            <h4 className="title total">Contact Information</h4>
+                            <p className="user-details total">
                                 Rishikesh Kumar (2105209@kiit.ac.in)
                             </p>
-                            <form 
-                            action=""
-                            className="d-flex gap-15 flex-wrap justify-content-between"
+                            <h4 className="mb-3">Shipping Address</h4>
+
+                            <form
+                                action=""
+                                className="d-flex gap-15 flex-wrap justify-content-between"
                             >
                                 <div className="w-100">
-                                    <select name="" 
-                                    className="form-control form-select" 
-                                    id="">
+                                    <select name=""
+                                        className="form-control form-select"
+                                        id="">
                                         <option value="" selected disabled>
                                             Select Country
                                         </option>
@@ -59,27 +61,27 @@ const Checkout = () => {
                                     <input type="text" placeholder="Apartment, Suite, etc" className="form-control" />
                                 </div>
                                 <div className="flex-grow-1" >
-                                    <input type="text"  placeholder="City" className="form-control" />
+                                    <input type="text" placeholder="City" className="form-control" />
                                 </div>
                                 <div className="flex-grow-1">
-                                    <select 
-                                        name="" 
-                                        className="form-control form-select" 
+                                    <select
+                                        name=""
+                                        className="form-control form-select"
                                         id="">
-                                            <option value="" selected disabled>
-                                                Select State
-                                            </option>
+                                        <option value="" selected disabled>
+                                            Select State
+                                        </option>
                                     </select>
                                 </div>
                                 <div className="flex-grow-1">
-                                    <input type="text"  placeholder="Zipcode" className="form-control" />
+                                    <input type="text" placeholder="Zipcode" className="form-control" />
                                 </div>
                                 <div className="w-100">
                                     <div className="d-flex justify-content-between align-items-center">
                                         <Link to="/cart" className="text-darker">
                                             <BiArrowBack className="me-2" />
                                             Return to Cart
-                                            </Link>
+                                        </Link>
                                         <Link to="/cart" className="button">Continue to Shopping</Link>
                                     </div>
                                 </div>
@@ -89,33 +91,33 @@ const Checkout = () => {
                     <div className="col-5">
                         <div className="border-bottom py-4">
                             <div className="d-flex gap-10 mb-2 align-items-center">
-                            <div className="w-75 d-flex gap-10">
-                                <div className="w-25 position-relative">
-                                     <span 
-                                     style={{top:"-10px",right:"2px"}}
-                                     className="badge bg-secondary text-white rounded-cicle p-2 position-absolute"
-                                     > 1</span>
-                                     <img src={watch} className="img-fluid"  alt="product" />
+                                <div className="w-75 d-flex gap-10">
+                                    <div className="w-25 position-relative">
+                                        <span
+                                            style={{ top: "-10px", right: "2px" }}
+                                            className="badge bg-secondary text-white rounded-cicle p-2 position-absolute"
+                                        > 1</span>
+                                        <img src={watch} className="img-fluid" alt="product" />
+                                    </div>
+                                    <div>
+                                        <h5 className="total-price">Watch</h5>
+                                        <p className='total-price'>ewef9869k</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <h5 className="total-price">Watch</h5>
-                                    <p className='total-price'>ewef9869k</p>
+                                <div className="flex-grow-1">
+                                    <h5 className='total'>$100</h5>
                                 </div>
                             </div>
-                            <div className="flex-grow-1">
-                                <h5 className='total'>$100</h5>
-                            </div>
-                        </div>
                         </div>
                         <div className-="border-bottom py-4">
-                        <div className="d-flex justify-content-between align-items-center">
-                            <p className='total'>Subtotal</p>
-                            <p className='total-price'>$10000</p>
-                        </div>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <p className='mb-0 total'>Shipping</p>
-                            <p className='mb-0 total-price'>$10000</p>
-                        </div>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <p className='total'>Subtotal</p>
+                                <p className='total-price'>$10000</p>
+                            </div>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <p className='mb-0 total'>Shipping</p>
+                                <p className='mb-0 total-price'>$10000</p>
+                            </div>
                         </div>
                         <div className="d-flex justify-content-between align-items-center border-bottom py-4">
                             <h4 className='total'>Total</h4>

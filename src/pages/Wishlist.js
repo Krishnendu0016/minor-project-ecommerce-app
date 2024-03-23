a275cd60c121344b22e9642620e82dec682c64d4
Krishnendu0016 <16kdas@gmail.com>
2024-03-23 20:34:59 +0530

    update wishlist

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -5,14 +5,14 @@ import Container from '../com/Container';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserProductWishlist } from '../features/user/userSlice';
 const Wishlist = () => {
-    const dispatch=useDispatch();
-    useEffect(()=>{
+    const dispatch = useDispatch();
+    useEffect(() => {
         getWishlistFromDb();
-    },[])
-    const getWishlistFromDb=()=>{
+    }, [])
+    const getWishlistFromDb = () => {
         dispatch(getUserProductWishlist());
     }
-    const wishListState=useSelector((state)=>state.auth?.wishlist?.wishlist)
+    const wishListState = useSelector((state) => state.auth?.wishlist?.wishlist)
 
     return (
         <>
@@ -21,34 +21,31 @@ const Wishlist = () => {
             <Container class1="wishlist-wrapper home-wapper-2 py-5">
                 <div className="row">
                     {
-                        wishListState?.map((item,index)=>{
-                            return(
+                        wishListState?.map((item, index) => {
+                            return (
                                 <div className="col-3" key={index}>
-                                <div className="wishlist-card position-relative">
-                                    <img src="images/cross.svg"
-                                        alt="cross"
-                                        className="position-absolute cross img-fluid"
-                                    />
-                                    <div className="wishlist-card-image">
-                                        <img src={item?.images[0].url?item?.images[0].url:"images/watch.jpg"}
-                                            className="img-fluid w-100"
-                                            alt="watch" />
-                                    </div>
-                                    <div className="py-3 px-3">
-                                        <h5 className="title">
-                                            {item?.title}
+                                    <div className="wishlist-card position-relative">
+                                        <img src="images/cross.svg"
+                                            alt="cross"
+                                            className="position-absolute cross img-fluid"
+                                        />
+                                        <div className="wishlist-card-image">
+                                            <img src={item.images && item.images[0] && item.images[0].url ? item.images[0].url : "images/watch.jpg"}
+                                                className="img-fluid w-100"
+                                                alt="watch" />
+                                        </div>
+
+                                        <div className="py-3 px-3">
+                                            <h5 className="title">
+                                                {item?.title}
                                             </h5>
-                                        <h6 className="price">${item?.price}</h6>
+                                            <h6 className="price">${item?.price}</h6>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             )
                         })
                     }
-                   
-                   
-
-
                 </div>
             </Container>
         </>
@@ -58,4 +55,4 @@ const Wishlist = () => {
 export default Wishlist
 
 
- 
+

7da63727c2366863f4534ca026c73a7a9071df2b
Kush12203 <kushpatelvk@gmail.com>
2024-03-24 02:12:22 +0530

    Contact Section and Featured collection

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,32 @@ import BreadCrumb from '../com/BreadCrumb';
 import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
 import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
 import Container from '../com/Container';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { createQuery } from '../features/contact/contactSlice';
+
+const contactSchema = yup.object({
+  name: yup.string().required("Name is Required"),
+  email: yup.string().nullable().email("Email should be valid").required("Email is required"),
+  mobile: yup.string().default('').nullable().required("Mobile is required"),
+  comment: yup.string().default('').nullable().required("Comment is required"),
+});
+
 const Contact = () => {
+  const dispatch =useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      mobile: '',
+      email: '',
+      comment:''
+    },
+    validationSchema:contactSchema,
+    onSubmit: values => {
+     dispatch(createQuery({name:values.name,email:values.email,mobile:values.mobile,comment:values.comment}))
+    },
+  });
   return (
     <>
       <Meta title={"Contact Us"} />
@@ -26,37 +51,73 @@ const Contact = () => {
             <div className="contact-inner-wrapper d-flex justify-content-between ">
               <div>
                 <h3 className="contact-title mb-4">Contact</h3>
-                <form action="" className="d-flex flex-column gap-15">
+                <form action="" onSubmit={formik.handleSubmit} className="d-flex flex-column gap-15">
                   <div>
                     <input
                       type="text"
                       className="form-control"
                       placeholder="Name"
+                      name="name"
+                      onChange={formik.handleChange("name")}
+                      onBlur={formik.handleBlur("name")}
+                      value={formik.values.name}
                     />
+                  <div className="errors">
+                    {
+                      formik.touched.name && formik.errors.name
+                    }
+                  </div>
                   </div>
                   <div>
                     <input
                       type="email"
                       className="form-control"
                       placeholder="Email"
+                      name="email"
+                      onChange={formik.handleChange("email")}
+                      onBlur={formik.handleBlur("email")}
+                      value={formik.values.email}
                     />
+                     <div className="errors">
+                    {
+                      formik.touched.email && formik.errors.email
+                    }
+                    </div>
                   </div>
                   <div>
                     <input
                       type="tel"
                       className="form-control"
                       placeholder="Mobile Number"
+                      name="mobile"
+                      onChange={formik.handleChange("mobile")}
+                      onBlur={formik.handleBlur("mobile")}
+                      value={formik.values.mobile}
                     />
+                     <div className="errors">
+                    {
+                      formik.touched.mobile && formik.errors.mobile
+                    }
+                    </div>
                   </div>
                   <div>
                     <textarea
-                      name=""
+                    
                       id=""
                       className="w-100 form-control"
                       cols="30"
                       rows="4"
                       placeholder="Comments"
+                      name="comment"
+                      onChange={formik.handleChange("comment")}
+                      onBlur={formik.handleBlur("comment")}
+                      value={formik.values.comment}
                     ></textarea>
+                    <div className="errors">
+                    {
+                      formik.touched.comment && formik.errors.comment
+                    }
+                    </div>
                   </div>
                   <div>
                     <button className="button border-0">Submit</button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -201,10 +201,56 @@ const Home = () => {
           <div className="col-12">
             <h3 className="section-heading">Featured Collection</h3>
           </div>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {
+            Array.isArray(productState) && productState.map((item, index) => {
+              return item.tags === "featured" ? (
+                <div key={index} className={"col-3"}>
+                  <Link
+                    // to={`${location.pathname == "/"
+                    // ? "/product/:id"
+                    // : location.pathname == "/product/:id"
+                    //   ? "/product/:id"
+                    //   : ":id"
+                    // }`} 
+                    className="product-card position-relative">
+                    <div className="wishlist-icon position-absolute">
+                      <button className='border-0 bg-transparent' onClick={(e) => { addToWish(item?._id) }}> <img src={wish} alt="wishlist" /></button>
+                    </div>
+                    <div className="product-image">
+                      <img src={item?.images?.[0]?.url} className="img-fluid  mx-auto" alt="product image" width={160} />
+                      {/* <img src={watch} className="img-fluid " alt="product image" /> */}
+
+                      <img src={watch2} className="img-fluid d-block mx-auto" alt="product image" width={160} />
+                      {/* <img src={watch2} className="img-fluid" alt="product image" /> */}
+                    </div>
+                    <div className="product-details">
+                      <h6 className='brand'>{
+                        item?.brand
+                      }</h6>
+                      <h5 className="product-title">
+                        {
+                          item?.title
+                        }
+                      </h5>
+                      <ReactStars count={5} size={24} value={item?.totalrating.toString()} edit={false} activeColor="#ffd700"
+                      />
+
+                      <p className="price">₹{
+                        item?.price
+                      }</p>
+                    </div>
+                    <div className="action-bar position-absolute">
+                      <div className="d-flex flex-column gap-15">
+                        <button className='border-0 bg-transparent'> <img src={prodcompare} alt="compare" /></button>
+                        <button className='border-0 bg-transparent'> <img src={view} alt="view" /></button>
+                        <button className='border-0 bg-transparent'><img src={addcart} alt="addcart" /></button>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ) : null;
+            })
+          }
         </div>
       </Container>
 

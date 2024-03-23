1580ab6cdfd325a32509ff0fc0133c120619c834
Krishnendu0016 <16kdas@gmail.com>
2024-03-24 01:30:54 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,9 +22,9 @@ import { addToWishlist } from '../features/products/productSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     getallProducts();
-  },[])
+  }, [])
   const productState = useSelector((state) => state.product.product);
   const getallProducts = () => {
     dispatch(getAllProducts());
@@ -282,14 +282,14 @@ const Home = () => {
         <div className="row">
           {
             Array.isArray(productState) && productState.map((item, index) => {
-              if (item.tags ==='special'){
-                return <SpecialProduct key={index} 
-                brand={item?.brand}
-                title={item?.title}
-                totalrating={item?.totalrating.toString()}
-                price={item?.price}
-                sold={item?.sold}
-                quantity={item?.quantity}
+              if (item.tags === 'special') {
+                return <SpecialProduct key={index}
+                  brand={item?.brand}
+                  title={item?.title}
+                  totalrating={item?.totalrating.toString()}
+                  price={item?.price}
+                  sold={item?.sold}
+                  quantity={item?.quantity}
                 />
               }
             })
@@ -305,60 +305,57 @@ const Home = () => {
           </div>
         </div>
         <div className="row">
-        {
+          {
             Array.isArray(productState) && productState.map((item, index) => {
-              if (item.tags ==="popular"){
-                return 
-                <div 
-                key={index}
-                className={"col-3"}>
-             <Link 
-               // to={`${location.pathname == "/"
-               // ? "/product/:id"
-               // : location.pathname == "/product/:id"
-               //   ? "/product/:id"
-               //   : ":id"
-               // }`} 
-               className="product-card position-relative">
-               <div className="wishlist-icon position-absolute">
-                 <button className='border-0 bg-transparent' onClick={(e)=>{addToWish(item?._id)}}> <img src={wish} alt="wishlist" /></button>
-               </div>
-               <div className="product-image">
-                 <img src={item?.images?.[0]?.url} className="img-fluid  mx-auto" alt="product image" width={160} />
-                 {/* <img src={watch} className="img-fluid " alt="product image" /> */}
-                
-                 <img src={watch2} className="img-fluid d-block mx-auto" alt="product image" width={160} />
-                 {/* <img src={watch2} className="img-fluid" alt="product image" /> */}
-               </div>
-               <div className="product-details">
-                 <h6 className='brand'>{
-                     item?.brand
-                   }</h6>
-                 <h5 className="product-title">
-                   {
-                     item?.title
-                   }
-                 </h5>
-                 <ReactStars count={5} size={24} value={item?.totalrating.toString()} edit={false} activeColor="#ffd700"
-                 />
-                 
-                 <p className="price">₹{
-                     item?.price
-                   }</p>
-               </div>
-               <div className="action-bar position-absolute">
-                 <div className="d-flex flex-column gap-15">
-                   <button className='border-0 bg-transparent'> <img src={prodcompare} alt="compare" /></button>
-                   <button className='border-0 bg-transparent'> <img src={view} alt="view" /></button>
-                   <button className='border-0 bg-transparent'><img src={addcart} alt="addcart" /></button>
-                 </div>
-               </div>
-             </Link>
-           </div>
-              }
+              return item.tags === "popular" ? (
+                <div key={index} className={"col-3"}>
+                  <Link
+                    // to={`${location.pathname == "/"
+                    // ? "/product/:id"
+                    // : location.pathname == "/product/:id"
+                    //   ? "/product/:id"
+                    //   : ":id"
+                    // }`} 
+                    className="product-card position-relative">
+                    <div className="wishlist-icon position-absolute">
+                      <button className='border-0 bg-transparent' onClick={(e) => { addToWish(item?._id) }}> <img src={wish} alt="wishlist" /></button>
+                    </div>
+                    <div className="product-image">
+                      <img src={item?.images?.[0]?.url} className="img-fluid  mx-auto" alt="product image" width={160} />
+                      {/* <img src={watch} className="img-fluid " alt="product image" /> */}
+
+                      <img src={watch2} className="img-fluid d-block mx-auto" alt="product image" width={160} />
+                      {/* <img src={watch2} className="img-fluid" alt="product image" /> */}
+                    </div>
+                    <div className="product-details">
+                      <h6 className='brand'>{
+                        item?.brand
+                      }</h6>
+                      <h5 className="product-title">
+                        {
+                          item?.title
+                        }
+                      </h5>
+                      <ReactStars count={5} size={24} value={item?.totalrating.toString()} edit={false} activeColor="#ffd700"
+                      />
+
+                      <p className="price">₹{
+                        item?.price
+                      }</p>
+                    </div>
+                    <div className="action-bar position-absolute">
+                      <div className="d-flex flex-column gap-15">
+                        <button className='border-0 bg-transparent'> <img src={prodcompare} alt="compare" /></button>
+                        <button className='border-0 bg-transparent'> <img src={view} alt="view" /></button>
+                        <button className='border-0 bg-transparent'><img src={addcart} alt="addcart" /></button>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ) : null;
             })
           }
-       
+
         </div>
       </Container>
       <Container class1="marque-wrapper home-wapper-2 py-5">
